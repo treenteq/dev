f057b64bc8c49c001e47061f7c7a0d1ea3d5ef2c
rahulbarmann <itsrahulbarman1@gmail.com>
2025-02-14 00:09:36 +0530

    feat: declare types for next-auth session and user

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            id?: string;
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+        };
+        accessToken?: string;
+    }
+
+    interface User {
+        id: string;
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+    }
+}
